72be72341cc036be056f5a9e91b98b2a7da8ede9
Nishant Kumar <nishant.trash@gmail.com>
2017-08-19 18:45:50 +0530

    .

---
+++ LambdaDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LambdaDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action Hello = () => Console.WriteLine("Hello");
+            Hello();
+            Console.ReadKey(true);
+        }
+    }
+}
