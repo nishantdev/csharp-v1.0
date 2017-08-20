81e035290d6a6575df54f3a40b3d8d24ebb3328a
Nishant <nishant.trash@gmail.com>
2017-08-20 17:26:42 +0530

    .

---
+++ LINQDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQDemo
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var people = GenerateListOfPeople();
+
+            foreach(var enumP in people.Where(x => x.FirstName.StartsWith("E")))
+            Console.WriteLine(enumP.FirstName);
+
+            Console.ReadKey(true);
+        }
+
+        public static List<Person> GenerateListOfPeople()
+        {
+            var people = new List<Person>();
+
+            people.Add(new Person { FirstName = "Eric", LastName = "Fleming", Occupation = "Dev", Age = 24 });
+            people.Add(new Person { FirstName = "Steve", LastName = "Smith", Occupation = "Manager", Age = 40 });
+            people.Add(new Person { FirstName = "Brendan", LastName = "Enrick", Occupation = "Dev", Age = 30 });
+            people.Add(new Person { FirstName = "Jane", LastName = "Doe", Occupation = "Dev", Age = 35 });
+            people.Add(new Person { FirstName = "Samantha", LastName = "Jones", Occupation = "Dev", Age = 24 });
+
+            return people;
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Occupation { get; set; }
+        public int Age { get; set; }
+    }
+}
