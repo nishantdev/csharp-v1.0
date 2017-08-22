c5d4637d4a21391495541846f8252fdaf96afc13
Nishant <nishant.trash@gmail.com>
2017-08-22 18:29:22 +0530

    .

---
+++ ArgumentsDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ArgumentsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(string arg in args)
+            Console.WriteLine(arg);
+            Console.ReadKey(true);
+        }
+    }
+}
