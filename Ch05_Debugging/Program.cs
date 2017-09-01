63a78ddf89b42ab941263d94977d97faaa062f4a
Nishant <nishant.trash@gmail.com>
2017-09-01 16:42:12 +0530

    .

---
+++ Ch05_Debugging/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+
+namespace Ch05_Debugging
+{
+    class Program
+    {
+        static double Add(double a, double b)
+        {
+
+            return a * b;
+
+        }
+
+        static void Main(string[] args)
+        {
+            double a = 10.50;
+            double b = 20.50;
+
+            var c = Add(a, b);
+
+            WriteLine($"{a} + {b} = {c}");
+
+            ReadKey(true);
+        }
+    }
+}
