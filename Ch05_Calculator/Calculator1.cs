96a8c3dd3417706d69557071e0da0d3e8b24a05c
Nishant <nishant.trash@gmail.com>
2017-09-10 14:19:36 +0530

    .

---
+++ Ch05_Calculator/Calculator1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ch05_Calculator
+{
+    public class Calculator1
+    {
+        public double Add(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
